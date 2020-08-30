3cd602c402388b260a3c8fc359c5050c5f707574
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-08-30 02:39:51 +0100

    Home Component Separate, Fragment Added

--- src/App.vue
+++ src/App.vue
@@ -24,41 +24,7 @@
           <div
             class="col-12 col-lg-8 d-flex flex-column justify-content-center py-5"
           >
-            <div class="f-24 mb-4 text-uppercase">Hi There, I'm</div>
-            <div class="f-64 mb-3">Ikechukwu Okonkwo</div>
-            <div class="f-24 f-blue mb-3">GRAPHICS + UI/UX DESIGNER</div>
-            <div class="f-18 mb-5 fw-325">
-              I am a creative with experience in cooperate and interface design.
-              My collaborative skills has been demonstrated through successful
-              completion of projects
-            </div>
-            <div class="icons-holder">
-              <BaseLink href="#" class="icon"
-                ><span
-                  class="iconify"
-                  data-icon="whh:dribbblealt"
-                  data-inline="false"
-                ></span> </BaseLink
-              ><BaseLink href="#" class="icon"
-                ><span
-                  class="iconify"
-                  data-icon="whh:behance"
-                  data-inline="false"
-                ></span> </BaseLink
-              ><BaseLink href="#" class="icon"
-                ><span
-                  class="iconify"
-                  data-icon="whh:linkedin"
-                  data-inline="false"
-                ></span> </BaseLink
-              ><BaseLink href="#" class="icon"
-                ><span
-                  class="iconify"
-                  data-icon="whh:twitter"
-                  data-inline="false"
-                ></span>
-              </BaseLink>
-            </div>
+            <Home></Home>
           </div>
           <div
             class="col-12 col-lg-4 d-flex flex-column justify-content-start align-items-end"
@@ -102,10 +68,12 @@
 <script>
   import HelloWorld from "./components/HelloWorld.vue";
   import BaseLink from "./components/BaseLink.vue";
+  import Home from "./views/Home.vue";
 
   export default {
     name: "App",
     components: {
+      Home,
       HelloWorld,
       BaseLink,
     },
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <Fragment>
+    <div class="f-24 mb-4 text-uppercase">Hi There, I'm</div>
+    <div class="f-64 mb-3">Ikechukwu Okonkwo</div>
+    <div class="f-24 f-blue mb-3">GRAPHICS + UI/UX DESIGNER</div>
+    <div class="f-18 mb-5 fw-325">
+      I am a creative with experience in cooperate and interface design. My
+      collaborative skills has been demonstrated through successful completion
+      of projects
+    </div>
+    <div class="icons-holder">
+      <BaseLink href="#" class="icon"
+        ><span
+          class="iconify"
+          data-icon="whh:dribbblealt"
+          data-inline="false"
+        ></span> </BaseLink
+      ><BaseLink href="#" class="icon"
+        ><span
+          class="iconify"
+          data-icon="whh:behance"
+          data-inline="false"
+        ></span> </BaseLink
+      ><BaseLink href="#" class="icon"
+        ><span
+          class="iconify"
+          data-icon="whh:linkedin"
+          data-inline="false"
+        ></span> </BaseLink
+      ><BaseLink href="#" class="icon"
+        ><span
+          class="iconify"
+          data-icon="whh:twitter"
+          data-inline="false"
+        ></span>
+      </BaseLink>
+    </div>
+  </Fragment>
+</template>
+<script>
+  import { Fragment } from "vue-fragment";
+  export default {
+    name: "Home",
+    components: {
+      Fragment,
+    },
+  };
+</script>
