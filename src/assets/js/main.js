9218dfc052be9f86a855e5ee8c60adc7e9c09837
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-09-03 15:35:43 +0100

    pwa added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,26 +1,26 @@
-document.body.style.cssText += "cursor: none !important;";
-const mouseIndicator = document.querySelector("#mouseindicator");
-document.addEventListener("mouseover", (e) => {
-  mouseIndicator.style.visibility = "visible";
-});
-document.addEventListener("mouseleave", (e) => {
-  mouseIndicator.style.visibility = "hidden";
-});
-document.addEventListener("mousemove", (e) => {
-  mouseIndicator.style.top = `${e.clientY - 4}px`;
-  mouseIndicator.style.left = `${e.clientX - 4}px`;
-});
-//   All links
-document.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("mousemove", () => {
-    mouseIndicator.classList.add("hovering");
-  });
-  link.addEventListener("mouseleave", () => {
-    mouseIndicator.classList.remove("hovering");
-  });
-});
-document.querySelectorAll("a *").forEach((link) => {
-  link.addEventListener("mousemove", () => {
-    mouseIndicator.classList.add("hovering");
-  });
-});
+// document.body.style.cssText += "cursor: none !important;";
+// const mouseIndicator = document.querySelector("#mouseindicator");
+// document.addEventListener("mouseover", (e) => {
+//   mouseIndicator.style.visibility = "visible";
+// });
+// document.addEventListener("mouseleave", (e) => {
+//   mouseIndicator.style.visibility = "hidden";
+// });
+// document.addEventListener("mousemove", (e) => {
+//   mouseIndicator.style.top = `${e.clientY - 4}px`;
+//   mouseIndicator.style.left = `${e.clientX - 4}px`;
+// });
+// //   All links
+// document.querySelectorAll("a").forEach((link) => {
+//   link.addEventListener("mousemove", () => {
+//     mouseIndicator.classList.add("hovering");
+//   });
+//   link.addEventListener("mouseleave", () => {
+//     mouseIndicator.classList.remove("hovering");
+//   });
+// });
+// document.querySelectorAll("a *").forEach((link) => {
+//   link.addEventListener("mousemove", () => {
+//     mouseIndicator.classList.add("hovering");
+//   });
+// });
